51a913ce2a7baf1c6c414428dbbd447b43b2907c
sunnydays15 <152016127+sunnydays15@users.noreply.github.com>
2023-12-17 08:52:46 +0000

    Good time

---
+++ December2023/learningArr1-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    char str[] = {'G','o','o','d',' ','t','i','m','e'};
+
+    for(int i=0; i<9; i++){
+        printf("%c", str[i]);
+    }
+    printf("\n");
+}
