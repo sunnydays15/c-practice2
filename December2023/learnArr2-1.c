8e4421f7ebe0c691b78e909a82a05769f0174eb7
sunnydays15 <152016127+sunnydays15@users.noreply.github.com>
2023-12-17 09:06:03 +0000

    영단어 길이 계산

--- December2023/learnArr2-1.c
+++ December2023/learnArr2-1.c
@@ -1,15 +1,4 @@
 #include <stdio.h>
-int main()
-{
-    char str[50];
-    printf("영단어 입력 : ");
-    scanf("%s", str);
-
-    int i=0;
-    while(str[i]!='\0'){
-        i++;
-    }
-    printf("%d", i);
 
     // int Len = sizeof(str)/sizeof(char);
     // printf("%d \n", Len);
