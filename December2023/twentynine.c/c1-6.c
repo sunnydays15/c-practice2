d40755ed468e5268bea443ccd3af1d97adbc4ee1
sunnydays15 <152016127+sunnydays15@users.noreply.github.com>
2023-12-29 09:58:33 +0000

    도전 1-6 문제 리뷰

---
+++ December2023/twentynine.c/c1-6.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int second;
+    printf("초(second) 입력 : ");
+    scanf("%d", &second);
+
+    printf("[h:%d, m:%d, s:%d]\n", second/3600,(second%3600)/60, (second%3600)%60);
+}
