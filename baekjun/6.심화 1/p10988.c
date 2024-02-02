51bec542f56e04cef0cc07390bb9233fa3a7bce4
sunnydays15 <152016127+sunnydays15@users.noreply.github.com>
2024-02-02 12:17:33 +0000

    팰린드롬인지 확인하기

---
+++ baekjun/6.심화 1/p10988.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int palindrome(int front, int back, char* ptr){
+    while(1){
+        if(front==back || front+1==back){
+            return 1;
+        }
+        else if(ptr[front]==ptr[back]){
+            front++;
+            back--;
+        }
+        else
+            return 0;
+    }
+}
+
+int main(){
+    char str[100];
+    scanf("%s", str);
+
+    int len;
+    int front=0;
+    for(int i=0; str[i]!=0; i++)
+        len=i;
+
+    printf("%d\n", palindrome(front, len, str));
+
+    return 0;
+}
---
+++ baekjun/6.심화 1/p2444-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int N;
+    scanf("%d", &N);
+
+    for(int i=0; i<N; i++){
+        char arr[i+N];
+        for(int j=0; j<i+N; j++){
+            arr[j]=' ';
+        }
+        
+    }
+}
