3d8e8432c4722bdcb4a628920431c09af4f63446
sunnydays15 <152016127+sunnydays15@users.noreply.github.com>
2024-02-13 15:29:00 +0000

    단어 공부

--- baekjun/6.심화 1/p1157.c
+++ baekjun/6.심화 1/p1157.c
@@ -1,4 +1,40 @@
 #include <stdio.h>
+#include <string.h>
+void Count(char* word){
+    int arr[26];
+    int len = strlen(word);
+
+    for(int i=0; i<len; i++){
+        if(word[i]-'a'<0){
+            arr[word[i]-'A']++;
+        }
+        else{
+            arr[word[i]-'a']++;
+        }
+    }
+
+    int max;
+    int maxCheck=0;
+    for(int i=0; i<26; i++){
+        if(maxCheck<arr[i]){
+            max=i;
+            maxCheck=arr[i];
+        }
+    }
+    for(int i=0; i<26; i++){
+        if(maxCheck==arr[i] && max!=i){
+            printf("?\n");
+            return;
+        }
+    }
+    printf("%C\n", max+'A');
+}
+
 int main(){
-    
+    char word[1000000];
+    scanf("%s", word);
+
+    Count(word);
+
+    return 0;
 }
