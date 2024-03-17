32d87599b91f35ee65ea8ab8e3d81c1287b70669
sunnydays15 <152016127+sunnydays15@users.noreply.github.com>
2024-03-17 00:30:27 +0000

    baekjoon p2751-py

---
+++ baekjoon/Python/13. 정렬/p2751.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+num_list = []
+for _ in range(N):
+    num_list.append(int(input()))
+
+num_list.sort()
+
+for i in num_list:
+    print(i)
