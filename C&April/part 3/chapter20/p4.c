7ad954095966b39bee1f292851466bf6f4a87bea
sunnydays15 <152016127+sunnydays15@users.noreply.github.com>
2024-04-15 11:45:00 +0000

    p20-4

---
+++ C&April/part 3/chapter20/p4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+    srand((int)time(NULL));
+
+    printf("주사위 1의 결과 %d\n", rand()%6+1);
+    printf("주사위 2의 결과 %d\n", rand()%6+1);
+    return 0;
+}
